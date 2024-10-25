f52db6d8b8cd59faf959c270e8355ac4cd50839e
s4turnru4n <138148427+s4turnru4n@users.noreply.github.com>
2024-10-25 16:28:09 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,20 @@
+criacartão(
+    'programação',
+    "O que é javascript?",
+    "o javascript é uma linguagem de programação"
+)
+criacartão(
+    "geografia",
+    "qual a capital da frança?",
+     'a capital da frança é paris'
+)
+criacartão(
+    'programação',
+     'oque e uma função',
+     'uma função é um bloco de codigo que execulta alguma tarefa'
+)
+criacartão(
+    categoria,
+    pergunta,
+    resposta
+)
