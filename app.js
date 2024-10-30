8fe3a6c12ea3d6d344fb42f562a2001bc0d18ce9
s4turnru4n <138148427+s4turnru4n@users.noreply.github.com>
2024-10-30 17:12:53 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,23 @@
+ function  criacartão(categoria, pergunta, resposta){
+ let container= document.getElementById('container')
+ let cartão= document.creatElement('article')
+ cartão.classname= 'cartão'
+
+ cartão.inerHTMl =
+
+´ <div class="cartão__conteudo">
+ <h3>programação</h3>
+ <div class="cartão__conteudo__pergunta">
+            qual a capital da frança?
+            <div class="cartão__conteudo__resposta">
+              a capital da frança é paris
+            </div>
+ </div>
+</div> ´
+
+
+
+
+
+
+}
